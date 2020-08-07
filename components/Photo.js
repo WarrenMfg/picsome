2361f1b47c79bb7a6ef58323940143f178ce25f3
WarrenMfg <warrenmfg@icloud.com>
2020-08-07 12:58:52 -0400

    add hover icons

--- components/Photo.js
+++ components/Photo.js
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Photo({ className, photo }) {
+  const [ isHovered, setIsHovered ] = useState(false);
+
   return (
-    <div className={`image-container ${className || ''}`}>
-      <img src={photo.url} className='image-grid' />
+    <div
+      className={`image-container${className}`}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <img src={photo.url} className='image-grid-item' />
+      {
+        isHovered &&
+        <>
+          <i className='ri-heart-line favorite' />
+          <i className='ri-add-circle-line cart' />
+        </>
+      }
     </div>
   );
 }
--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,9 @@
 export function getClass(i) {
   if (i % 5 === 0) {
-    return 'big';
+    return ' big';
   } else if (i % 6 === 0) {
-    return 'wide';
+    return ' wide';
+  } else {
+    return '';
   }
 }
