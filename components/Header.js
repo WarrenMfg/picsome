092f48b819ac4464dee31c29732d25d06377e0f9
WarrenMfg <warrenmfg@icloud.com>
2020-08-07 17:38:07 -0400

    add cart icon change

--- components/Header.js
+++ components/Header.js
@@ -1,17 +1,54 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import withGlobalStore from './withGlobalStore';
 
-function Header() {
+function Header({ state: { cart } }) {
+  const className =
+    cart.length > 0 ? 'ri-shopping-cart-fill ri-fw ri-2x' : 'ri-shopping-cart-line ri-fw ri-2x';
+
   return (
     <header>
       <Link to='/'>
         <h2>Pic Some</h2>
       </Link>
       <Link to='/cart'>
-        <i className='ri-shopping-cart-line ri-fw ri-2x' />
+        <i className={className} />
       </Link>
     </header>
   );
 }
 
-export default Header;
+Header.propTypes = {
+  state: PropTypes.shape({
+    loading: PropTypes.bool.isRequired,
+    photos: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+        isFavorite: PropTypes.bool.isRequired
+      })
+    ).isRequired,
+    error: PropTypes.object,
+    dispatch: PropTypes.func.isRequired,
+    cart: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string.isRequired,
+        url: PropTypes.string.isRequired,
+        isFavorite: PropTypes.bool.isRequired
+      })
+    ).isRequired
+  })
+};
+
+const memoized = React.memo(Header, (prevProps, nextProps) => {
+  if (prevProps.state.cart.length === nextProps.state.cart.length) {
+    return true;
+  } else if (prevProps.state.cart.length >= 1 && nextProps.state.cart.length >= 1) {
+    return true;
+  } else {
+    return false;
+  }
+});
+
+export default withGlobalStore(memoized);
